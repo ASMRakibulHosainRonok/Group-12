0e32d1b917f7c2df0dcdf0da2faecd9f109c1cb2
Ronok <ronokhossain1@gmail.com>
2024-04-14 22:38:20 +0600

    14

---
+++ DBMS_Group-12/Agri_exp.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agricultural Expert</title>
+    <link rel="stylesheet" href="Agri_exp.css">
+</head>
+<body>   
+    <h2><b>About Me</b></h2>
+    <img class = "new-img" src= "Image/51.JPEG"><br>
+     <h6>
+        Hi,<br> 
+        This is ABC. Currently I'm an agricultural expert. I have a lot of experience to working with renowed company.
+     </h6>
+    <br>
+    <hr>
+    <ul>
+        <h2><b>Qualification</b></h2>
+        <img class = "new-image" src= "Image/60.jpg"><br>
+        <p> An advanced degree in agriculture, agribusiness or a related field required;<br>
+            A minimum of 10 years of experience in agriculture; <br>
+            Experience in conducting evaluations and/or assessments, of similar size and scope in development countries preferred; <br>
+            Experience in designing and supervising agriculture household level surveys preferred; <br>
+            Strong communication, analytical, writing, and interpersonal skills; <br>
+            Excellent team management skills; and <br>
+            Professional proficiency in written and spoken French and English required</p><br>
+    </ul>
+    <hr class="common-hr">
+<div class="work">
+    <h2><b>Work Experience</b></h2>
+    <img class = "work-img" src= "Image/214.JPEG"><br>
+    <p>In my Professional life I have couple of work experience with world most renowed company.</p> <br>
+        <h4>American Society of Agronomy (ASA), 2010-2016 <br>
+            Crop Science Society of America (CSSA), 2016-2021 <br>
+            National Association of County Agricultural Agents (NACAA), 2021-Present</h4>
+        </div>
+    <hr class="common-hr">
+</body>
+</html>
